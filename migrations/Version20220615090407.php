74ff5e37c580b1f7619d561008c32974c1ca5d6d
Nicolas Grekas <nicolas.grekas@gmail.com>
2022-06-15 11:26:12 +0200

    admin area

--- migrations/Version20220615090407.php
+++ migrations/Version20220615090407.php
@@ -20,10 +20,10 @@ final class Version20220615090407 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql(<<<'SQL'
-INSERT INTO admin (id, username, roles, password)
-VALUES (nextval('admin_id_seq'), 'admin', '["ROLE_ADMIN"]', '$2y$13$.uXjJ6He.kqzepXdwxL1GuAbvZhoUGlJ1lKa.dmPjpEkSOy1HAOTi')
-SQL
-);
+            INSERT INTO admin (id, username, roles, password)
+            VALUES (nextval('admin_id_seq'), 'admin', '["ROLE_ADMIN"]', '$2y$13$.uXjJ6He.kqzepXdwxL1GuAbvZhoUGlJ1lKa.dmPjpEkSOy1HAOTi')
+            SQL
+        );
     }
 
     public function down(Schema $schema): void
